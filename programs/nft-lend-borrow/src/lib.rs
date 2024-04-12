9eb5e75549b6476887bd961af6a0d66911e644e5
thongmessi <166774074+thongmessi@users.noreply.github.com>
2024-04-12 08:53:24 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("BuVCDxkrcDWLDfbh3WMUmvotuvAmvzzR1rAsmrHZRXRe");
 
 #[program]
 pub mod nft_lend_borrow {
